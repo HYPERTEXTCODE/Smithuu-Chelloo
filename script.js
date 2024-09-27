244168caebf13d7993a6bb8790aabbb3c5e9835c
Pradeep P <109416622+HYPERTEXTCODE@users.noreply.github.com>
2024-09-28 00:24:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,8 @@ const musicPaths = [
     "music/day11.mp3", // Path to music for Sept 22, 2024
     "music/day12.mp3", // Path to music for Sept 22, 2024
     "music/day13.mp3", // Path to music for Sept 22, 2024
-    "music/day14.mp3", // Path to music for Sept 22, 20
+    "music/day14.mp3", // Path to music for Sept 22, 2024
+    "music/day15.mp3", // Path to music for Sept 22, 2024
     // Add paths to 363 more music files here
 ];
 
