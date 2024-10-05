879eabe4a057b2eafe6ace1fcb64739db12c845b
Pradeep P <109416622+HYPERTEXTCODE@users.noreply.github.com>
2024-10-06 01:22:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const musicPaths = [
     "music/day17.mp3", // Path to music for Sept 30, 2024
     "music/day18.mp3", //
     "music/day19.mp3", //
-    "music/day20.mp3", // Path to music for Oct 01, 2024
+    "music/day23.mp3", // Path to music for Oct 01, 2024
 ];
 
 // Function to calculate the difference between the first day and the selected/current day
